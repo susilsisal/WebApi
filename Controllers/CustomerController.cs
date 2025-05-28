3c467fb90ec77d7ad2332d5513ae4ecc68ce801e
Susil Poudel <susilsisal@gmail.com>
2025-05-28 22:15:48 +0545

    custome location done

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -14,6 +14,50 @@ namespace ApiHost.Controllers
 
             return Ok();
         }
+        [HttpPost]
+        [Route("api/add-customers-location")]
+        public async Task<IActionResult> AddCustomerLocation([FromBody] Usp_Insert_Customer_Location_ProcParams model)
+        {
+            try
+            {
+                await DbHelper.CallPostgresProcedureAsync("usp_insert_customer_location_proc", model);
+                return Ok(new { message = "Customer location added successfully." });
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if you have logging in place
+                // _logger.LogError(ex, "Error inserting customer location");
+
+                return StatusCode(500, new
+                {
+                    error = "An error occurred while adding customer location.",
+                    details = ex.Message
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/get-customer-location")]
+        public async Task<IActionResult> GetCustomerLocation(int c_id)
+        {
+            try
+            {
+                var list =await DbHelper.QueryPostgresFunctionAsync<CusotmerLocation>("usp_get_customerlocation", new { c_id});
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if you have logging in place
+                // _logger.LogError(ex, "Error inserting customer location");
+
+                return StatusCode(500, new
+                {
+                    error = "",
+                    details = ex.Message
+                });
+            }
+        }
+
 
         [HttpGet]
         [Route("api/get-customers")]
@@ -54,6 +98,31 @@ namespace ApiHost.Controllers
     }
 
 
+    public class Usp_Insert_Customer_Location_ProcParams
+    {
+        public int P_Customer_Id { get; set; }
+        public decimal P_Latitude { get; set; }
+        public decimal P_Longitude { get; set; }
+        public string P_Address { get; set; }
+        public string P_Label { get; set; }
+        public DateTime P_Created_At { get; set; }
+        public string P_Nearest_Landmark { get; set; }
+        public string P_Remarks { get; set; }
+    }
+
+    public class CusotmerLocation
+    {
+
+public	int	location_id	{ get; set; }
+public	int	customer_id	{ get; set; }
+public	float	latitude	{ get; set; }
+public	float	longitude	{ get; set; }
+public	string	address	{ get; set; }
+public	string	label	{ get; set; }
+public	DateTime	created_at	{ get; set; }
+public	string	nearest_landmark	{ get; set; }
+public	string	remarks	{ get; set; }
+    }
     public class usp_Insert_Customers
     {
         public string name { get; set; }
